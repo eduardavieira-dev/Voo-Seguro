0ab9151cb96ed8651b8e1430a86fa542481907e9
Laura Pontara <lauracplopes@gmail.com>
2024-11-18 18:11:50 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <cstdlib>
+#include <locale.h>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "portuguese");
+    int escolha;
+
+    do {
+        cout << "Escolha o que deseja fazer:\n";
+        cout << "1 - Cadastrar passageiro\n";
+        cout << "2 - Cadastrar tripulação\n";
+        cout << "3 - Cadastrar voo\n";
+        cout << "4 - Cadastrar assento\n";
+        cout << "5 - Cadastrar reserva\n";
+        cout << "6 - Baixa em reservas\n";
+        cout << "7 - Pesquisa\n";
+        cout << "8 - Consulta ao programa de fidelidade\n";
+        cout << "0 - Sair\n";
+        cin >> escolha;
+
+        switch (escolha) {
+        case 1:
+            system("cls");
+            cout << "escolha 1\n" << endl;
+            break;
+
+        case 2:
+            system("cls");
+            cout << "escolha 2\n" << endl;
+            break;
+
+        case 3:
+            system("cls");
+            cout << "escolha 3\n" << endl;
+            break;
+
+        case 4:
+            system("cls");
+            cout << "escolha 4\n" << endl;
+            break;
+
+        case 5:
+            system("cls");
+            cout << "escolha 5\n" << endl;
+            break;
+
+        case 6:
+            system("cls");
+            cout << "escolha 6\n" << endl;
+            break;
+
+        case 7:
+            system("cls");
+            cout << "escolha 7\n" << endl;
+            break;
+
+        case 8:
+            system("cls");
+            cout << "escolha 9\n" << endl;
+            break;
+
+        case 0:
+            system("cls");
+            cout << "Programa encerrado\n" << endl;
+            exit(0);
+            break;
+
+        default:
+            system("cls");
+            cout << "Escolha invalida!\n" << endl;
+            break;
+        }
+    } while (escolha != 0);
+
+    return 0;
+}
