a642bfce20445394c43830f08f7a0c4105cebe20
Eduarda Vieira <159597766+eduardavieira-dev@users.noreply.github.com>
2024-11-28 07:22:18 -0300

    Update Pesquisa.h

--- Pesquisa.h
+++ Pesquisa.h
@@ -1,16 +1,49 @@
-#ifndef TRIPULACAO2_H
-#define TRIPULACAO2_H
+#ifndef PESQUISA_H
+#define PESQUISA_H
 #include <string>
 #include <vector>
+#include "Passageiro.h"
+#include "Tripulacao.h"
 
 using namespace std;
 
 class Pesquisa{
+
+    private:
+    int IdPassageiro;
+    string NomePassageiro;
+    int IdTripulante;
+    string NomeTripulante;
+
+
     public:
+
+  // Métodos Setters
+    void setCodPassageiro(int codPassageiro);
+    void setNomePassageiro(string nomePassageiro);
+    void setCodTripulacao(int codTripulante);
+    void setNomeTripulacao(string nomeTripulante);
+
+   // Métodos Getters
+    int getCodPassageiro();
+    int getCodTripulante();
+    string getNomePassageiro();
+    string getNomeTripulante();
+
+
+ 
+  // Métodos de pesquisa
+
+    void pesquisarNomePassageiro(string NomePassageiro); 
+    void pesquisarIdPassageiro(int IdPassageiro);                
+    void pesquisarNomeTripulante(string NomeTripulante);
+    void pesquisarIdTripulante(int IdTripulante);  
+
+
     void pesquisarTripulacao();
     void pesquisarPassageiro();
 };
 
 
 
-#endif // TRIPULACAO2_H
+#endif // PESQUISA_H3
